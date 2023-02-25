05ffbf884faf92b343ecc62960617abe50dd7993
Adrian Soto <juliioadrian@gmail.com>
2023-02-25 11:27:57 -0700

    Ejercicios Javascript

---
+++ codigo.js
@@ -0,0 +1,19 @@
+let precio= 45;
+let cantidad= 5;
+
+let total= precio * cantidad;
+
+console.log(total)
+
+let num1= 23;
+let num2= 23;
+
+let suma= num1+num2;
+let resta= num1-num2;
+let multiplicacion= num1*num2;
+let division= num1/num2;
+
+console.log("Suma: ",suma)
+console.log("Resta: ",resta)
+console.log("Multiplicacion: ",multiplicacion)
+console.log("Division: ",division)
