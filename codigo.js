7d7945611b3b4c41f80c13e03c74ddea9d26b09a
Adrian Soto <juliioadrian@gmail.com>
2023-02-25 11:30:21 -0700

    Ejercicios Javascript 2

--- codigo.js
+++ codigo.js
@@ -1,3 +1,39 @@
+// let nombre = "Jesus";
+// let apellido = "Fernandez";
+
+// Imprimir valores en la consola
+// console.log(nombre);
+// console.log(apellido);
+
+// Reasignar una variable el valor
+
+/* 
+comnetarios de multiples
+lineas
+*/
+// nombre = "Omar";
+// console.log(nombre);
+
+// Declaracion de una variable
+let variable;
+// Asignacion de una variable
+variable = "algun dato";
+// Declaracion y una asignacion
+let variable2 = "otro dato";
+// Reasignacion de una variable
+variable2 = "reasignacion de dato";
+
+let nombre = "Adrian";
+let apellido = "Soto";
+let edad = 27;
+let fechaDeNacimiento = "1995-05-20";
+let soltero = false;
+let activo = false;
+
+// console.log(nombre + " " + apellido);
+// console.log(edad);
+// console.log(fechaDeNacimiento);
+// console.log(soltero);
 let precio= 45;
 let cantidad= 5;
 
